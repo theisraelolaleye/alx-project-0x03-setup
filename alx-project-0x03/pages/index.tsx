e3e8b75bdf703e79c40d764dceae4dfd254fb7bb
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-26 16:50:45 +0100

    2. Setting up our landing page with Imperative routing

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,22 +1,35 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+interface PageRouteProps {
+  pageRoute: string
+}
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export default function Home() {
+  const router = useRouter()
 
-const Home = () => {
+  // Imeperative routing with useRouter
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false })
+  }
+
   return (
-    <div>
-      <h1> Welcome to Splash App </h1>
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+      {/* Welcome Message */}
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        Welcome to Splash App!
+      </h1>
+      <p className="text-lg text-gray-600 mb-8">
+        Your one-stop platform for everything AI you need. Start exploring by
+        navigating to our features below.
+      </p>
+
+      {/* Navigation Options */}
+      <div className="flex gap-6">
+        <Button action={() => routeToNextPage({ pageRoute: '/generate-text-ai' })} buttonLabel="Generate Text" buttonBackgroundColor="blue" />
+        <Button action={() => routeToNextPage({ pageRoute: '/text-to-image' })} buttonLabel="Text to Image" buttonBackgroundColor="green" />
+        <Button action={() => routeToNextPage({ pageRoute: '/counter-app' })} buttonLabel="Contact us" buttonBackgroundColor="orange" />
+      </div>
     </div>
   );
-};
-
-export default Home;
+}
