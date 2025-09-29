95770b9592dc893263bb185f7a328fc1b367bf52
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-29 13:17:17 +0100

    h1 element fixes made

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
-      {/* text-4xl font-bold text-gray-800 mb-4 */}
-      <h1 className="">Welcome to Splash App!</h1>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Splash App!</h1>
+      {/* <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to Splash App!</h1> */}
       <p className="text-lg text-gray-600 mb-8">
         Your one-stop platform for everything AI you need. Start exploring by
         navigating to our features below.
